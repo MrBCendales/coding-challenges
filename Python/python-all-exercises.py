25b57a88819b8afeb575af92a35d8516bf6c111a
Bryan Cendales <bn.cendales10@gmail.com>
2026-03-09 15:10:18 +0900

    Basic Data types started

--- Python/python-all-exercises.py
+++ Python/python-all-exercises.py
@@ -1,4 +1,5 @@
 import numpy as np
+
 # Task / prod - sum
 # You are given a 2-D array with dimensions NXM.S
 # Your task is to perform the sum tool over axis 0 and then find the product of that result.
@@ -9,15 +10,15 @@ import numpy as np
 # Compute the sum along axis 0. Then, print the product of that sum.
 
 
-first_line= input()
+first_line = input()
 
 N, M = [int(x) for x in first_line.split()]
-data_array=[]
+data_array = []
 
 for i in range(N):
     data_array.append([int(x) for x in input().split()])
 
-print (np.prod(np.sum(np.array(data_array), axis = 0)))
+print(np.prod(np.sum(np.array(data_array), axis=0)))
 
 # Task / min max
 # You are given a 2-D array with dimensions NXM.
@@ -30,12 +31,12 @@ print (np.prod(np.sum(np.array(data_array), axis = 0)))
 
 N, M = [int(x) for x in input().split()]
 
-data_array=[]
+data_array = []
 for i in range(N):
     data_array.append([int(x) for x in input().split()])
 
-np_array=np.array(data_array)
-print(np.max(np.min(np_array, axis = 1)))
+np_array = np.array(data_array)
+print(np.max(np.min(np_array, axis=1)))
 
 # Task
 # You are given a 2-D array of size NXM.
@@ -55,10 +56,9 @@ for i in range(N):
 
 np_array = np.array(data_array)
 
-print(np.mean(np_array, axis = 1))
-print(np.var(np_array, axis = 0))
+print(np.mean(np_array, axis=1))
+print(np.var(np_array, axis=0))
 print(round(np.std(np_array), 11))
-
 
 
 # Task
@@ -78,12 +78,12 @@ for i in range(N):
     A.append([int(x) for x in input().split()])
 for i in range(N):
     B.append([int(x) for x in input().split()])
-    
+
 array_A = np.array(A)
 array_B = np.array(B)
 
 print(np.dot(array_A, array_B))
-            
+
 
 # Task
 # You are given two arrays: A and B.
@@ -101,3 +101,54 @@ B = np.array([int(x) for x in input().split()])
 print(np.inner(A, B))
 print(np.outer(A, B))
 
+
+# Task
+# You are given the coefficients of a polynomial P.
+# Your task is to find the value of P at point x.
+# Input Format
+# The first line contains the space separated value of the coefficients in P.
+# The second line contains the value of x.
+# Output Format
+# Print the desired value
+
+
+space_values = [float(x) for x in input().split()]
+x_value = int(input())
+
+print(np.polyval(space_values, x_value))
+
+# Task
+# You are given a square matrix A with dimensions NXN. Your task is to find the determinant. Note: Round the answer to 2 places after the decimal.
+# Input Format
+# The first line contains the integer N.
+# The next N lines contains the N space separated elements of array A.
+# Output Format
+# Print the determinant of A.
+
+
+N = int(input())
+a_matrix = [[float(x) for x in input().split()] for i in range(N)]
+
+print(round(np.linalg.det(a_matrix), 2))
+
+
+####################################### BASIC DATA TYPES#########################################
+
+# Given the participants' score sheet for your University Sports Day, you are required to find the runner-up score. You are given n scores. Store them in a list and find the score of the runner-up.
+# Input Format
+# The first line contains n. The second line contains an array A[] of n integers each separated by a space.
+# Constraints
+# • 2≤n≤ 10
+# • -100 ≤ Ai
+# ≤ 100
+# Output Format
+# Print the runner-up score.
+
+if __name__ == "__main__":
+    n = int(input())
+    arr = map(int, input().split())
+
+    list_arr = list(arr)
+    sorted_arr = sorted(set(list_arr))
+
+    print(sorted_arr[-2])
